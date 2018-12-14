cebfaf342f85be504fdf9d6848932f7941020a1f
suya <dlsenfl3@gmail.com>
2018-12-14 16:20:33 +0900

    Add files via upload

--- UDPTest02/Unit2.cpp
+++ UDPTest02/Unit2.cpp
@@ -22,16 +22,16 @@ void __fastcall TForm2::btStartClick(TObject *Sender)
 {
 	if(!IdUDPServer1->Active){
 
-		sHostIP    	 = edHostIP->Text;
-		usHostPort   = StrToInt(edHostPort->Text);
-		usClientPort = StrToInt(edClientPort->Text);
+		sHostIP    	 = edHostIP->Text;                                       // 데이터를 전달받을 서버Ip
+		usHostPort   = StrToInt(edHostPort->Text);                           // 데이터를 전달받을 서버Port
+		usClientPort = StrToInt(edClientPort->Text);                         // 데이터를 전달하는 클라이언트 Port
 		IdUDPServer1->DefaultPort = usClientPort;
 
 		ShowMessage("HostPort: "+edHostPort->Text+
 					"\nHostIP "+edHostIP->Text+
 					"ClientPort: "+IntToStr(IdUDPServer1->DefaultPort));
 
-		IdUDPServer1->Active = true;
+		IdUDPServer1->Active = true;                                         // Active = true; 를 마지막에 선언해줘야 위에 세팅한 값들이 적용된상태로 통신시작.
 	}
 }
 //---------------------------------------------------------------------------
@@ -41,7 +41,7 @@ void __fastcall TForm2::btRequestClick(TObject *Sender)
 	TstHeader stHeader;
 	TstTail stTail;
 	int iIndex  = 0;
-
+	//======= 메모리 초기화=======
 	ZeroMemory(byBuf, sizeof(byBuf));
 	ZeroMemory(&stHeader, sizeof(stHeader));
 	ZeroMemory(&stTail, sizeof(stTail));
@@ -54,9 +54,10 @@ void __fastcall TForm2::btRequestClick(TObject *Sender)
 
 	stTail.byETX1	  = 0x10;
 	stTail.byETX2	  = 0x03;
+	//byBuf 배열에 메모리 초기화한 헤더구조체입력.
 	CopyMemory(byBuf, &stHeader, sizeof(stHeader));
 	iIndex = sizeof(stHeader);
-
+	// byOpCode에 따라 데이터부분 초기화 및 byBuf배열의 헤더가 입력된 부분 뒤에 입력.
 	switch(stHeader.byOpCode){
 
 		case 0x06:
@@ -75,8 +76,11 @@ void __fastcall TForm2::btRequestClick(TObject *Sender)
 			ShowMessage("None of CodeNo");
 			break;
 	}
+	// 테일구조체 초기화 및 byBuf 배열 마지막 순서에 입력.
 	CopyMemory(byBuf+iIndex, &stTail, sizeof(stTail));
 	iIndex += sizeof(stTail);
+
+	// byBuf 배열을 서버로 전달하기위한 부분
 	TByteDynArray aBuf;
 	aBuf.Length = iIndex;
 	CopyMemory(&(aBuf[0]), byBuf, iIndex);
@@ -99,11 +103,11 @@ void __fastcall TForm2::edClientPortKeyPress(TObject *Sender, System::WideChar &
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm2::IdUDPServer1UDPRead(TIdUDPListenerThread *AThread, const TIdBytes AData,
-		  TIdSocketHandle *ABinding)
+		  TIdSocketHandle *ABinding)		// UDP데이터를받을때 실행하는 함수
 {
 	BYTE byBuf[1024];
 	ZeroMemory(byBuf, sizeof(byBuf));
-	BytesToRaw(AData, byBuf, AData.Length);                                             // 데이터부분 이상한 값 전달??
+	BytesToRaw(AData, byBuf, AData.Length);     // AData를 AData.Length 길이만큼 byBuf에 입력.
 	UnicodeString sTemp;
 
 	TstHeader stHeader;
@@ -111,7 +115,7 @@ void __fastcall TForm2::IdUDPServer1UDPRead(TIdUDPListenerThread *AThread, const
 	TstTail	  stTail;
 	TstData06 stData06;
 	TstData07 stData07;
-
+	// 요청에 응답하는 구조체헤더 초기화
 	ZeroMemory(&stHeaderStore, sizeof(stHeaderStore));
 	stHeaderStore.bySTX1   = 0x10;
 	stHeaderStore.bySTX2   = 0x02;
@@ -123,7 +127,7 @@ void __fastcall TForm2::IdUDPServer1UDPRead(TIdUDPListenerThread *AThread, const
 	CopyMemory(&stHeader, byBuf, sizeof(stHeader));
 	iIndex = sizeof(stHeader);
 	if(stHeader.byOpCode == 0x06){
-		if(stHeader.wDataLen > 0){
+		if(stHeader.wDataLen > 0){    // 응답받은 데이터(byBuf)를 각각의 (로컬)헤더, 데이터, 테일부분에 저장
 			if(stHeader.wDataLen != sizeof(stData06)) return;
 			ZeroMemory(&stData06, sizeof(stData06));
 			CopyMemory(&stData06, byBuf+iIndex, sizeof(stData06));
@@ -131,10 +135,9 @@ void __fastcall TForm2::IdUDPServer1UDPRead(TIdUDPListenerThread *AThread, const
 			ZeroMemory(&stTail, sizeof(stTail));
 			CopyMemory(&stTail, byBuf+iIndex, sizeof(stTail));
 			iIndex += sizeof(stTail);
-
 		}else{
 			mmShow->Lines->Add("요청받음");
-			CopyMemory(byBuf, &stHeaderStore, sizeof(stHeaderStore));                         //wDataLen 0xf ???
+			CopyMemory(byBuf, &stHeaderStore, sizeof(stHeaderStore)); //wDataLen 값이 0xf ??? , 저장되는 16진수값은 0x0c
 //			ZeroMemory(&stData06, sizeof(stData06));
 			CopyMemory(byBuf+iIndex, &stData06, sizeof(stData06));
 			IdUDPServer1->SendBuffer(sHostIP, usHostPort, RawToBytes(&byBuf, sizeof(byBuf)));
@@ -160,11 +163,16 @@ void __fastcall TForm2::IdUDPServer1UDPRead(TIdUDPListenerThread *AThread, const
 	}else if(stHeader.byOpCode == 0x0B){
 	}
 
+
 	if(iIndex == sizeof(stHeader)+stHeader.wDataLen+sizeof(stTail)){
 		mmShow->Lines->Add("응답데이터 출력");
+		UnicodeString sData = "";
+		// byBuf에 저장되어있는 데이터를 16진수 표기의 UnicodeString으로 출력.
+		for(int i=0; i<iIndex; i++){
+			sData +=sTemp.sprintf(L"%02X ", byBuf[i]);  // 0을붙여서 (0포함)2자리수로 16진수 출력
+		}
+		mmShow->Lines->Add(sData);
 
-		sTemp.sprintf(L"%02X", byBuf);
-		mmShow->Lines->Add(sTemp);
 	}
 //	ZeroMemory(&stTail, sizeof(stTail));
 //	CopyMemory(&stTail, byBuf+iIndex, sizeof(stTail));
